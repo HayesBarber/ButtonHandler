67a8c3385f5c307821b8a118eb57fc287d87fff8
Hayes Barber <barberhayes@gmail.com>
2025-06-29 20:44:51 -0400

    add comments

--- src/ButtonHandler.h
+++ src/ButtonHandler.h
@@ -3,10 +3,30 @@
 
 #include <Arduino.h>
 
+/**
+ * Represents a debounced digital button.
+ */
 class Button {
 public:
+    /**
+     * Constructs a new Button instance.
+     */
     Button();
+
+    /**
+     * Initializes the button on a specified GPIO pin.
+     * 
+     * The pin will be configured as INPUT_PULLUP.
+     * 
+     * @param pin GPIO pin number.
+     * @param callback Function to call when the button is pressed (goes LOW).
+     */
     void init(int pin, void (*callback)(int));
+
+    /**
+     * Should be called regularly in the loop() to update button state.
+     * Triggers callback if the button press is valid and debounced.
+     */
     void update();
 
 private:
